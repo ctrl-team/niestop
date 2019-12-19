c0fb5eec4b512c489f5a2e7f4755e1fb51a7608a
Nimplex <39964594+Nimplex@users.noreply.github.com>
2019-12-19 20:25:43 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const button = document.getElementById("button");
+const lose = document.getElementById("lose");
+
+let losed = false;
+
+let czas = 0;
+function addTime() {
+  if (losed == false) {
+    czas += 1;
+    console.log("add");
+  }
+}
+
+button.addEventListener("mouseup", () => {
+  losed = true;
+  lose.style.visibility = "visible";
+  lose.innerHTML = `<p>Przegrałeś trzymałeś przez: ${czas}s</p>`;
+});
+button.addEventListener("mousedown", () => {
+  setInterval("addTime()", 1000);
+  losed = false;
+});
+
+if (losed == true) {
+  lose.style.visibility = "visible";
+  losed = true;
+} else {
+  lose.style.visibility = "hidden";
+  losed = false;
+}
